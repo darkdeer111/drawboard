8c451e4881c32e1c70c9c69cae89651838baf5d6
darkdeer111 <92444716+darkdeer111@users.noreply.github.com>
2022-06-26 14:43:35 +0800

    Add files via upload

---
+++ src/views/Other/page1.vue
@@ -0,0 +1,9 @@
+<template>
+	<h1>这是page1</h1>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
---
+++ src/views/Other/page2.vue
@@ -0,0 +1,9 @@
+<template>
+	<h1>这是pagepage2</h1>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
