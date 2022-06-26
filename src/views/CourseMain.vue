790935ffbfff8012c96bf0221a0b9487c9a52c1e
darkdeer111 <92444716+darkdeer111@users.noreply.github.com>
2022-06-26 14:40:31 +0800

    Create CourseMain.vue

---
+++ src/views/CourseMain.vue
@@ -0,0 +1,38 @@
+<template>
+	<el-container style="height: 100%">
+	  <el-aside width="auto">
+		  <course-aside></course-aside>
+	  </el-aside>
+	  <el-container>
+	    <el-header>
+			<course-header></course-header>
+		</el-header>
+		<course-tab></course-tab>
+	    <el-main>
+			<router-view></router-view>
+		</el-main>
+	  </el-container>
+	</el-container>
+</template>
+
+<script>
+	import CourseAside from '../components/CourseAside.vue'
+	import CourseHeader from '../components/CourseHeader.vue'
+	import CourseTab from '../components/CourseTab.vue'
+	export default{
+		components:{
+			CourseAside,
+			CourseHeader,
+			CourseTab
+		}
+	}
+</script>
+
+<style>
+	.el-aside{
+		background-color: #99CC33;
+	}
+	.el-header{
+		background-color: #aaff7f;
+	}
+</style>
